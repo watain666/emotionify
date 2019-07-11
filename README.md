e9159149d55f0eef6f0100899e9e01cdf1634463
Brent Vollebregt <volleys2468@gmail.com>
2019-07-11 21:46:30 +1200

    Build 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# emotionify
-Create an emotion gradiented Spotify playlist
---
+++ precache-manifest.fb929b9cfb659b5e9d99755f68b0738a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bbee524bed4ada1660291dd252afd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7e2da2039603f428e6",
+    "url": "/static/css/main.27548ef9.chunk.css"
+  },
+  {
+    "revision": "2f356f87ab833357d65c",
+    "url": "/static/js/2.197612f5.chunk.js"
+  },
+  {
+    "revision": "1a7e2da2039603f428e6",
+    "url": "/static/js/main.d7f2c3e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed08e733d6a6f2ab96ad8af6d15c5aff",
+    "url": "/static/media/banner.ed08e733.png"
+  },
+  {
+    "revision": "5fbd211044c8a5a2ee79cdf62eb0d13f",
+    "url": "/static/media/sort-comparison.5fbd2110.png"
+  },
+  {
+    "revision": "d154494c9913b044d7a2abb63ee91828",
+    "url": "/static/media/spotify-logo-round.d154494c.png"
+  }
+]);
